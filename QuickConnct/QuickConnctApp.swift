2697c1b3768c40312ead969f4e402e8830427558
Colinzinho <developmentcolinzinho@gmail.com>
2025-05-04 21:22:03 +0200

    Initial Commit

---
+++ QuickConnct/QuickConnctApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuickConnctApp.swift
+//  QuickConnct
+//
+//  Created by Colinzinho Development on 04.05.2025.
+//
+
+import SwiftUI
+
+@main
+struct QuickConnctApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
